ad13b974f1d563030e875472ffc7d9f9103fc257
paco salomon <paco.salomon@insa-lyon.fr>
2018-03-26 17:48:01 +0200

    Base pour le main

---
+++ tp7.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+//#include "String.h"
+
+int main(int argc, char* argv[]){
+  std::cout << "Hello World !" << std::endl;
+  return 0;
+}
