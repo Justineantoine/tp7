482b9691fe83c1dad15918321cc8b06e8a8ef8e2
J.Antoine <justineantoine17@gmail.com>
2018-03-30 08:33:39 +0200

    Add destructor

--- String.cpp
+++ String.cpp
@@ -2,10 +2,18 @@
 
 size_t String::MAX_SIZE;
 
-//Constructors/Destructor
+//Constructors
 
 //Getters
 
 //Operators
 
 //Methodes
+
+//Destructor
+String::~String()
+{
+delete []c_str_;
+c_str_ = nullptr;
+size_ = 0;
+}
--- String.h
+++ String.h
@@ -5,13 +5,13 @@ class String{
 	public:
 	//Attributs
 	size_t size_;
-	char* c_str_;
+	char* c_str_; //pointeur vers la premiere case du tableau de char
 	size_t capacity_;
 	
 	protected:
 	static size_t MAX_SIZE const;	
 
-	//Constructors/Destructor
+	//Constructors
 
 
 	//Operators
@@ -20,7 +20,8 @@ class String{
 	//Methodes
 
 
-
+	//Destructor
+	~String();
 };
 
 
